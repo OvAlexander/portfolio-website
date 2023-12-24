24549d6736425d958af993f5d709ccd55b7fd48b
OvAlexander <alexanderleeov@gmail.com>
2023-12-24 04:16:24 -0800

    Created React Portfolio App

---
+++ src/data.js
@@ -0,0 +1,56 @@
+export const projects = [
+  {
+    title: "Life Dashboard",
+    subtitle: "MERN Stack",
+    description: "",
+    image: "./mine-clicker-game.gif",
+    link: "https://github.com/OvAlexander/life-dashboard",
+  },
+  {
+    title: "Mine Clicker Game",
+    subtitle: "HTML, CSS, and Javascript",
+    description: "",
+    image: "./mine-clicker-game.gif",
+    link: "https://github.com/OvAlexander/mining_clicker_game",
+  },
+  {
+    title: "YouTube ELO Rater",
+    subtitle: "Python",
+    description: "",
+    image: "./mine-clicker-game.gif",
+    link: "https://github.com/OvAlexander/YoutubePlaylistELO",
+  },
+  {
+    title: "Document Text to Speech",
+    subtitle: "Python, Google Cloud APIs",
+    description: "",
+    image: "./mine-clicker-game.gif",
+    link: "",
+  },
+];
+export const hardware_projects = [
+  {
+    title: "BANSHEE UAV",
+    subtitle: "Mechatronics POD",
+    description: "Robotic Arm utilizing computer vision and TCP/IP Protocol",
+    image: "./BTP.gif",
+    link: "https://www.bansheeuav.tech/home",
+  },
+  {
+    title: "React Tracks",
+    subtitle: "React and Python",
+    description: "",
+    image: "./BTP.gif",
+    link: "",
+  },
+];
+export const skills = ["Javascript", "Python", "C/C++", "Robotics"];
+export const testimonials = [
+  {
+    name: "React Reserve",
+    quote: "MERN Stack",
+    company:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./project-1.gif",
+  },
+];
