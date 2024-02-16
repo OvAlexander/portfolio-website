c25f717e39a3b69c1fb3fe72f5f4c247e187b7b6
OvAlexander <alexanderleeov@gmail.com>
2024-02-15 17:27:00 -0800

    feat: Added custom favicon

--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -1,36 +1,36 @@
-import React, { useState, useEffect } from "react";
-import Navbar from "../components/Navbar";
-import Post from "../components/Post";
-import axios from 'axios';
+// import React, { useState, useEffect } from "react";
+// import Navbar from "../components/Navbar";
+// import Post from "../components/Post";
+// import axios from 'axios';
 
-// GET request
-const fetchData = async () => {
-  try {
-      const response = await axios.get('http://localhost:5000/api/data');
-      console.log(response.data);
-  } catch (error) {
-      console.error(error);
-  }
-};
+// // GET request
+// const fetchData = async () => {
+//   try {
+//       const response = await axios.get('http://localhost:5000/api/data');
+//       console.log(response.data);
+//   } catch (error) {
+//       console.error(error);
+//   }
+// };
 
-export default function Blog() {
-  // const [data, setData] = useState(null);
+// export default function Blog() {
+//   // const [data, setData] = useState(null);
 
-    useEffect(() => {
-        fetchData();
-    }, []);
-  return (
-    <section className="text-blue-400 bg-primary body-font">
-    <main className="">
-      <Navbar />
-      <div className="grid grid-cols-2 gap-10 flex flex-row min-h-screen justify-center items-center">
-        {/* {data} */}
-        <Post />
-        <Post />
-        <Post />
-        <Post />
-      </div>
-    </main>
-    </section>
-  );
-}
+//     useEffect(() => {
+//         fetchData();
+//     }, []);
+//   return (
+//     <section className="text-blue-400 bg-primary body-font">
+//     <main className="">
+//       <Navbar />
+//       <div className="grid grid-cols-2 gap-10 flex flex-row min-h-screen justify-center items-center">
+//         {/* {data} */}
+//         <Post />
+//         <Post />
+//         <Post />
+//         <Post />
+//       </div>
+//     </main>
+//     </section>
+//   );
+// }
