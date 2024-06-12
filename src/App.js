9b65cfde61b8182c05bde7d346d633783356fa40
OvAlexander <alexanderleeov@gmail.com>
2024-06-12 11:02:33 -0700

    test: added base urls

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Research from "./pages/Research";
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename = {process.env.PUBLIC_URL}>
       <Routes>
         <Route path="/" element={<Home />}/>
         <Route path="/blog" element={<Blog />}/>
