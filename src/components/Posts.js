e2b59a57fd9cf8d61bfe2b9c759b5c285cf27c69
OvAlexander <alexanderleeov@gmail.com>
2024-06-12 12:50:56 -0700

    chore: fixed typos in blog post

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -16,17 +16,17 @@ const Posts = () => {
         },
         {
         title: "Portfolio Dev Log 6/11/24",
-        body: `Flexible and accessibility is a crucial aspect of any product! Today my main focus on developing my portfolio was increasing the accesiblity and responsivness
+        body: `Flexible and accessibility is a crucial aspect of any product! Today my main focus on developing my portfolio was increasing the accessibility and responsiveness
         of the site this was all demoed using the blog site! Through today's development it is clear that going forward I should branch off main and create "testing"
-        enviroments allowing me to make large sweeping changes and have checkpoints this will allow me to do comparisions of old and new code while also being able to rollback anytime
-        Going forward into the development of this site I will attempt to intergrate more responsiveness from the start and look into other accesiblity features such as alt text.
-        Aside from responsivness changes I added a new tab for research where I will display my conferences and publications.`,
+        environments allowing me to make large sweeping changes and have checkpoints this will allow me to do comparisons of old and new code while also being able to rollback anytime
+        Going forward into the development of this site I will attempt to integrate more responsiveness from the start and look into other accessibility features such as alt text.
+        Aside from responsiveness changes I added a new tab for research where I will display my conferences and publications.`,
         author: "Alex",
         imgUrl: "./blog_reponsive.png",
         },
         {
         title: "Portfolio Dev Log 6/10/24",
-        body: `After several months of on an off development of my portfolio website, I have finally added a blog section and intend to make the website multipaged!
+        body: `After several months of on an off development of my portfolio website, I have finally added a blog section and intend to make the website multi-paged!
         While I would like to link the website to a backend due to the way I have deployed the website using Netlify it doesn't seem like it should be my number one
         priority. One resource that helped tremendously was this article from Medium https://medium.com/@reactcompany01/how-to-create-a-blog-app-with-reactjs-b8c0a56fcde1.
         Feeling very productive in this Google phone room!`,
